1f30277a1835c574d7dce71891cf17f3aeb79be4
Lauren Powers <laurenpowers@Laurens-MacBook-Air.local>
2022-10-27 11:30:00 -0400

    generating character photo

--- src/Simpson.jsx
+++ src/Simpson.jsx
@@ -4,16 +4,22 @@ import App from './App';
 function Simpson() {
   const [character, setCharacter] = useState("");
 
-  const handleClick = (e) => {
+  const handleClick = () => {
     fetch("https://thesimpsonsquoteapi.glitch.me/quotes")
       .then((res) => res.json())
-      .then((res) => setCharacter(res));
-  };
+      .then((res) => (
+        (document.querySelector("#image").src = res[0].image))
+       
+          .then((res) => setCharacter(res)))
+      }
     return (
       <div>
          <button onClick={handleClick} id="character">Click for character</button>
       
-        <div>{character.character}</div>
+        <div>
+          <h2 id="character"> </h2>
+          <img id="image" src="" /></div>
+      
       </div>
     );
   }
