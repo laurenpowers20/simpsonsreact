b08fa6be6172c2f0120ef8f99be3b041f304668b
Lauren Powers <laurenpowers@Laurens-Air.lan>
2022-12-06 14:59:17 -0500

    finalize

--- src/Simpson.jsx
+++ src/Simpson.jsx
@@ -1,27 +1,38 @@
 import React, { useState } from "react";
 import Modal from "./Modal";
 
-
 function Simpson() {
   const [character, setCharacter] = useState([]);
   const [showModal, setShowModal] = useState(false);
 
-  const getCharacters = async() => {
-    const response = await fetch("https://thesimpsonsquoteapi.glitch.me/quotes")
-    const json = await response.json()
-    setCharacter(json)
-  }
+  const getCharacters = async () => {
+    const response = await fetch(
+      "https://thesimpsonsquoteapi.glitch.me/quotes"
+    );
+    const json = await response.json();
+    setCharacter(json);
+  };
 
   if (character.length <= 0) {
-    return <><h1 className="homepage-title"> Click button for new character</h1>
-      <button onClick={getCharacters}>Get Character</button></>
+    return (
+      <>
+        <h1 className="homepage-title"> Click button for new quote</h1>
+        <button onClick={getCharacters}>Get Character</button>
+      </>
+    );
   } else {
     return (
       <div className="character">
         {character.map((simpsonscharacter) => {
-          return <div key={simpsonscharacter.character}>
-            <img src={simpsonscharacter.image} alt={simpsonscharacter.characer} className="image" />
-          </div>
+          return (
+            <div key={simpsonscharacter.character}>
+              <img
+                src={simpsonscharacter.image}
+                alt={simpsonscharacter.characer}
+                className="image"
+              />
+            </div>
+          );
         })}
         <Modal
           onClose={() => setShowModal(false)}
@@ -32,10 +43,8 @@ function Simpson() {
         <button onClick={() => setShowModal(true)}>Get info!</button>
         <button onClick={getCharacters}>New Character!</button>
       </div>
-    )
+    );
   }
 }
 
-
-
-export default Simpson
+export default Simpson;
